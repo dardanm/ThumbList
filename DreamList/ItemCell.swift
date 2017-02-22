06146027beef23700513f343fb62527576831119
Dardan Meha <mdardan20@gmail.com>
2017-02-21 19:40:39 -0500

    Connected views

---
+++ DreamList/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamList
+//
+//  Created by Dardan Meha on 2/21/17.
+//  Copyright © 2017 Dardan Meha. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var Title: UILabel!
+    @IBOutlet weak var Price: UILabel!
+    @IBOutlet weak var Details: UILabel!
+    
+
+}
